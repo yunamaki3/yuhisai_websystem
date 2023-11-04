81a0a9d4cca5d81c7395c33f8d0b6491a76fca9f
Yunamaki <makihana.2007@gmail.com>
2023-11-04 21:36:44 +0900

    complete detailpage

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  res.render('top.ejs');
+});
+
+app.get('/detail', (req, res) => {
+  res.render('detail.ejs');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
 });
 
 app.listen(3000);
